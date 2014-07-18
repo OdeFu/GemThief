f80118733ea51daa651f8251c5ba6e270c52a116
Olli Aalto <oaalto@gmail.com>
2014-07-18 13:24:01 +0300

    Add player to the map.

--- client/main.js
+++ client/main.js
@@ -7,7 +7,8 @@ Meteor.startup(function()
 var Game =
 {
     display: null,
-    map: {}
+    map: {},
+	player: null
 };
 
 Game.init = function ()
@@ -22,6 +23,7 @@ Game.init = function ()
 Game._generateMap = function ()
 {
     var digger = new ROT.Map.Digger();
+	var freeCells = [];
 
     var digCallback = function (x, y, value)
     {
@@ -33,17 +35,46 @@ Game._generateMap = function ()
 
         var key = x + "," + y;
         this.map[key] = ".";
+	    freeCells.push(key);
     };
     digger.create(digCallback.bind(this));
+
+	this._createPlayer(freeCells);
 };
 
 Game._drawWholeMap = function ()
 {
     for (var key in this.map)
     {
-        var parts = key.split(",");
-        var x = parseInt(parts[0]);
-        var y = parseInt(parts[1]);
-        this.display.draw(x, y, this.map[key]);
+	    if (this.map.hasOwnProperty(key))
+	    {
+		    var parts = key.split(",");
+		    var x = parseInt(parts[0]);
+		    var y = parseInt(parts[1]);
+		    this.display.draw(x, y, this.map[key]);
+	    }
     }
+
+	this.player.draw();
+};
+
+Game._createPlayer = function (freeCells)
+{
+	var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+	var key = freeCells.splice(index, 1)[0];
+	var parts = key.split(",");
+	var x = parseInt(parts[0]);
+	var y = parseInt(parts[1]);
+	this.player = new Player(x, y);
+};
+
+var Player = function (x, y)
+{
+	this._x = x;
+	this._y = y;
+};
+
+Player.prototype.draw = function ()
+{
+	Game.display.draw(this._x, this._y, "@", "#ff0");
 };
