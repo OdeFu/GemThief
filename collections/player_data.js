bd1650e653c9673673bde84048185d2e6e378d44
Olli Aalto <oaalto@gmail.com>
2014-07-23 12:24:01 +0300

    Reformat.

--- collections/player_data.js
+++ collections/player_data.js
@@ -1,51 +1,51 @@
 PlayerData = new Meteor.Collection("player_data");
 
 Meteor.methods(
+{
+	update: function (data)
 	{
-		update: function (data)
+		"use strict";
+
+		check(data.gems, Number);
+		check(data.moves, Number);
+		//check(data.gold, Number);
+
+		if (!this.userId)
 		{
-			"use strict";
-
-			check(data.gems, Number);
-			check(data.moves, Number);
-			//check(data.gold, Number);
-
-			if (!this.userId)
-			{
-				return 0;
-			}
-
-			var score = calculateScore(data);
-
-			var userData = PlayerData.findOne({ userId: this.userId });
-			if (userData)
-			{
-				userData.gold += data.gold;
-				if (userData.best_score < score)
-				{
-					userData.best_score = score;
-				}
-				userData.total_score += score;
-				userData.updated = new Date().getTime();
-				PlayerData.upsert(userData);
-			}
-			else
-			{
-				var newUserData =
-				{
-					userId: this.userId,
-					gold: data.gold,
-					best_score: score,
-					total_score: score,
-					created: new Date().getTime()
-				};
-				PlayerData.insert(newUserData);
-			}
-
-			data.score = score;
-			return data;
+			return 0;
 		}
-	});
+
+		var score = calculateScore(data);
+
+		var userData = PlayerData.findOne({ userId: this.userId });
+		if (userData)
+		{
+			userData.gold += data.gold;
+			if (userData.best_score < score)
+			{
+				userData.best_score = score;
+			}
+			userData.total_score += score;
+			userData.updated = new Date().getTime();
+			PlayerData.upsert(userData);
+		}
+		else
+		{
+			var newUserData =
+			{
+				userId: this.userId,
+				gold: data.gold,
+				best_score: score,
+				total_score: score,
+				created: new Date().getTime()
+			};
+			PlayerData.insert(newUserData);
+		}
+
+		data.score = score;
+		return data;
+	}
+});
 
 var calculateScore = function (data)
 {
