2e2fb89e9509fa2d34de5370ebcd88cf4482ff2a
Olli Aalto <oaalto@gmail.com>
2015-03-25 17:16:52 +0200

    Rename files.

--- packages/entities/package.js
+++ packages/entities/package.js
@@ -1,10 +1,10 @@
 Package.describe({
 	                 name: "entities",
-	                 version: "0.0.1",
+	                 version: "0.0.2"
                  });
 
 Package.onUse(function (api) {
 	api.versionsFrom("WINDOWS-PREVIEW@0.3.0");
 	api.use(["grigio:babel", "globals"]);
-	api.addFiles(["entity.jsx", "stairs.jsx", "tile.jsx", "map_features.jsx"]);
+	api.addFiles(["entity.es6.js", "stairs.es6.js", "tile.es6.js", "map_features.es6.js"]);
 });
