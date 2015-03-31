d8d5fdb10c7db4ca3075da4e0f6d8ec5183dda92
Olli Aalto <oaalto@gmail.com>
2015-03-31 20:04:27 +0300

    Time to start over.

--- client/main.es6.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Meteor.startup(function startup() {
-	// Check we're already logged in
-	if (Meteor.user()) {
-		GemThief.Game.init(false);
-	}
-});
-
--- packages/collections/collections.js
+++
@@ -1,3 +0,0 @@
-GemThief.DungeonData = new Mongo.Collection("dungeon");
-GemThief.Games = new Mongo.Collection("games");
-GemThief.PlayerData = new Mongo.Collection("player_data");
