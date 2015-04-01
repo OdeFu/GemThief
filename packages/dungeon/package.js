17a6acd19cf59cb83ed9c8d2decf33167b460a2a
Olli Aalto <oaalto@gmail.com>
2015-04-01 15:37:04 +0300

    Remove the display package.

--- packages/dungeon/package.js
+++ packages/dungeon/package.js
@@ -5,6 +5,6 @@ Package.describe({
 
 Package.onUse(function (api) {
 	api.versionsFrom("WINDOWS-PREVIEW@0.3.0");
-	api.use(["grigio:babel", "globals", "oaalto:rot", "entities", "display", "stevezhu:lodash", "map", "dwarf-ai"]);
+	api.use(["grigio:babel", "globals", "oaalto:rot", "entities", "stevezhu:lodash", "map", "dwarf-ai"]);
 	api.addFiles(["dungeon.es6.js", "map.es6.js", "path.es6.js", "map_display.es6.js"]);
 });
