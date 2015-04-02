2c05d07c4e875b9ad57385a9939e63c139a256b6
Olli Aalto <oaalto@gmail.com>
2015-04-02 11:29:33 +0300

    Add Messenger package and change the lodash package.

--- packages/globals/package.js
+++ packages/globals/package.js
@@ -1,11 +1,11 @@
 Package.describe({
-	                 name: "globals",
-	                 version: "0.0.2",
-	                 summary: "Define the global namespace for GemThief."
-                 });
+	name: "globals",
+	version: "0.0.3",
+	summary: "Define the global namespace for GemThief."
+});
 
 Package.onUse(function (api) {
-	api.versionsFrom("WINDOWS-PREVIEW@0.3.0");
+	api.versionsFrom("1.1");
 	api.addFiles(["globals.js", "object_utils.es6.js"]);
 	api.export("GemThief");
 	api.export("ObjectUtils");
